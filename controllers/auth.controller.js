a59eef0edecaa10e73d380193276c875be142f1e
theharism <chharis9999@gmail.com>
2025-06-22 13:22:05 +0500

    sign in google resp fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -267,7 +267,7 @@ exports.signInWithGoogle = async (req, res) => {
                 return res.status(200).json({
                     success: true,
                     message: 'User logged in successfully',
-                    token: token,
+                    data: { token } ,
                 });
             }
             else {
