043abbb3cd4fc951ebf42a0e0cc65f2ab8517b6a
theharism <chharis9999@gmail.com>
2025-06-27 01:30:56 +0500

    server sent push noti

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuG_EYVmx6RYyO6W4bTbqBfjV5ycK-c8c",
+  authDomain: "treksor-aa949.firebaseapp.com",
+  projectId: "treksor-aa949",
+  storageBucket: "treksor-aa949.firebasestorage.app",
+  messagingSenderId: "894029547682",
+  appId: "1:894029547682:web:6ca61d87a96d8f5cb4affb",
+  measurementId: "G-3GW32ZHB2C"
+};
+
+const app = initializeApp(firebaseConfig);
--- controllers/powerThought.controller.js
+++ controllers/powerThought.controller.js
@@ -1,11 +1,66 @@
 const PowerThought = require("../models/powerThought.model");
+const { Expo } = require('expo-server-sdk');
 const logger = require("../services/logger");
+const User = require('../models/user.model'); // Assuming you have a User model
+
+const expo = new Expo({
+  useFcmV1: true,
+});
 
 // Create a power thought
 exports.createPowerThought = async (req, res) => {
   try {
     const thought = await PowerThought.create(req.body);
     logger.info(`PowerThought created on ${thought.date}`);
+    // Create the messages that you want to send to clients
+    let messages = [];
+    const somePushTokens = await User.find().select("pushToken");
+    for (let pushToken of somePushTokens) {
+      // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+      // Check that all your push tokens appear to be valid Expo push tokens
+      if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`);
+        continue;
+      }
+
+      // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+      messages.push({
+        to: pushToken,
+        sound: 'default',
+        body: 'This is a test notification',
+        data: { withSome: 'data' },
+        // richContent: {
+        //   image: 'https://example.com/statics/some-image-here-if-you-want.jpg'
+        // },
+      })
+    }
+
+    // The Expo push notification service accepts batches of notifications so
+    // that you don't need to send 1000 requests to send 1000 notifications. We
+    // recommend you batch your notifications to reduce the number of requests
+    // and to compress them (notifications with similar content will get
+    // compressed).
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+      // Send the chunks to the Expo push notification service. There are
+      // different strategies you could use. A simple one is to send one chunk at a
+      // time, which nicely spreads the load out over time:
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          console.log(ticketChunk);
+          tickets.push(...ticketChunk);
+          // NOTE: If a ticket contains an error code in ticket.details.error, you
+          // must handle it appropriately. The error codes are listed in the Expo
+          // documentation:
+          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
     res.status(201).json({ success: true, data: thought });
   } catch (error) {
     logger.error("Error creating PowerThought:", error);
