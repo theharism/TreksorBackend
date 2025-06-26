8d2f36395dbd2ac57de4c86d3f695a64980e22f9
theharism <chharis9999@gmail.com>
2025-06-27 01:41:16 +0500

    final fix

--- controllers/powerThought.controller.js
+++ controllers/powerThought.controller.js
@@ -20,13 +20,13 @@ exports.createPowerThought = async (req, res) => {
 
       // Check that all your push tokens appear to be valid Expo push tokens
       if (!Expo.isExpoPushToken(pushToken.pushToken)) {
-        console.error(`Push token ${pushToken} is not a valid Expo push token`);
+        console.error(`Push token ${pushToken.pushToken} is not a valid Expo push token`);
         continue;
       }
 
       // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
       messages.push({
-        to: pushToken,
+        to: pushToken.pushToken,
         sound: 'default',
         body: 'This is a test notification',
         data: { withSome: 'data' },
